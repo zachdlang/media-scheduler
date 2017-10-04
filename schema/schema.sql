48377f16b7aa654e2855c4de7091f2fd14ee40c5
Zach Lang <zach.d.lang@gmail.com>
2017-10-04 20:00:18 +1300

    Renamed watcher table to enduser table

--- schema/schema.sql
+++ schema/schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS episode (
 	tvdb_id INTEGER NOT NULL
 )WITH OIDS;
 
-CREATE TABLE IF NOT EXISTS watcher (
+CREATE TABLE IF NOT EXISTS enduser (
 	id SERIAL primary key,
 	firstname TEXT NOT NULL,
 	surname TEXT NOT NULL,
@@ -28,13 +28,13 @@ CREATE TABLE IF NOT EXISTS watcher (
 CREATE TABLE IF NOT EXISTS watcher_tvshow (
 	id SERIAL primary key,
 	tvshowid INTEGER NOT NULL REFERENCES tvshow(id),
-	watcherid INTEGER NOT NULL REFERENCES watcher(id)
+	watcherid INTEGER NOT NULL REFERENCES enduser(id)
 )WITH OIDS;
 
 CREATE TABLE IF NOT EXISTS watcher_episode (
 	id SERIAL primary key,
 	episodeid INTEGER NOT NULL REFERENCES episode(id),
-	watcherid INTEGER NOT NULL REFERENCES watcher(id),
+	watcherid INTEGER NOT NULL REFERENCES enduser(id),
 	watched BOOLEAN NOT NULL DEFAULT FALSE
 )WITH OIDS; 
 
@@ -48,6 +48,6 @@ CREATE TABLE IF NOT EXISTS movie (
 CREATE TABLE IF NOT EXISTS watcher_movie (
 	id SERIAL primary key,
 	movieid INTEGER NOT NULL REFERENCES movie(id) ON DELETE CASCADE,
-	watcherid INTEGER NOT NULL REFERENCES watcher(id) ON DELETE CASCADE,
+	watcherid INTEGER NOT NULL REFERENCES enduser(id) ON DELETE CASCADE,
 	watched BOOLEAN NOT NULL DEFAULT FALSE
 )WITH OIDs;
