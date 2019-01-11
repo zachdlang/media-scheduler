27ee0034075c39f88eff792c991d369bebb56678
Zach Lang <zach.d.lang@gmail.com>
2019-01-12 00:52:52 +1300

    Makes username and password required for login POST request

--- web/schedule.py
+++ web/schedule.py
@@ -13,19 +13,21 @@ def login():
 	if request.method == 'POST':
 		ok = False
 		params = params_to_dict(request.form)
-		cursor = g.conn.cursor()
-		cursor.execute("""SELECT * FROM app.enduser WHERE TRIM(username) = TRIM(%s)""", (params['username'],))
-		if cursor.rowcount > 0:
-			resp = query_to_dict_list(cursor)[0]
-			ok, new_hash = g.passwd_context.verify_and_update(params['password'].strip(), resp['password'].strip())
-			if ok:
-				if new_hash:
-					cursor.execute("""UPDATE app.enduser SET password = %s WHERE id = %s""", (new_hash, resp['id'],))
-					g.conn.commit()
-				session.new = True
-				session.permanent = True
-				session['userid'] = resp['id']
-		cursor.close()
+
+		if params.get('username') is not None and params.get('password') is not None:
+			cursor = g.conn.cursor()
+			cursor.execute("""SELECT * FROM app.enduser WHERE TRIM(username) = TRIM(%s)""", (params['username'],))
+			if cursor.rowcount > 0:
+				resp = query_to_dict_list(cursor)[0]
+				ok, new_hash = g.passwd_context.verify_and_update(params['password'].strip(), resp['password'].strip())
+				if ok:
+					if new_hash:
+						cursor.execute("""UPDATE app.enduser SET password = %s WHERE id = %s""", (new_hash, resp['id'],))
+						g.conn.commit()
+					session.new = True
+					session.permanent = True
+					session['userid'] = resp['id']
+			cursor.close()
 			
 		if ok:
 			return redirect(url_for('schedule.home'))
